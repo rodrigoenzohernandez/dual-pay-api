4933513fc1942decaa7e98ba1caa626da1e5dea6
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-12-17 17:45:48 -0300

    add params to endpoint

--- src/controllers/bondController.js
+++ src/controllers/bondController.js
@@ -3,11 +3,17 @@ const get = require('../services/GET');
 const bondController = {
     async getBondPrice(req, res) {
 
-        const data = await get(res, 'https://www.allaria.com.ar/es/Bono/EspecieEvolutionGraph?tickers=GD30');
+        const name = req.query.name
+
+        const config = { params: { tickers: name } }
+        
+        const data = await get(res, 'https://www.allaria.com.ar/es/Bono/EspecieEvolutionGraph', config);
+
         const parsedData = JSON.parse(`${data.split('"dataProvider": ')[1].split('}],')[0]}}]`)
         const lastPrice = parsedData.pop()
 
         res.json(lastPrice)
+
     },
   };
   
