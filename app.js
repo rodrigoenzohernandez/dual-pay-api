0190c2f8a6b5d7577dc7f677129ca7da80c88bb1
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-12-17 13:20:45 -0300

    import bondsRouter

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./src/routes/index');
-var usersRouter = require('./src/routes/users');
+var bondsRouter = require('./src/routes/bonds');
+
 
 var app = express();
 
@@ -15,7 +16,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/bonds', bondsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
