211022950b026865581e3a5eb46c81d7844c3ad6
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-12-21 12:33:24 -0300

    change index message

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('hi');
+  res.send('welcome to dual-pay-api');
 });
 
 module.exports = router;
